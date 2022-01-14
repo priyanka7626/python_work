1c0195a6069b16fe3f92be8793b15d0897db452b
Priyanka <mail.priyankaaher@gmail.com>
2022-01-14 18:48:59 +0530

    hi

--- calculator_demo.py
+++ calculator_demo.py
@@ -2,6 +2,9 @@
 class Demo:
     def add(self):
         print("This is add function")
+    def sub(self)
+        print("This is sub function")
+
     def m(self):
         Demo.a=10
         print(Demo.a)
