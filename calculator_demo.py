6ff7f6b5a8ad1dc60ab19d6826ef898327b64ddf
Priyanka <priyanka21gorade@gmail.com>
2022-01-14 18:13:13 +0530

    first commit

---
+++ calculator_demo.py
@@ -0,0 +1,12 @@
+
+class Demo:
+    def m(self):
+        Demo.a=10
+        print(Demo.a)
+
+    def n(self):
+        print(a)  # 'a' is local variable of m()
+
+
+d = Demo()
+d.m()
---
+++ decorator_demo1.py
@@ -0,0 +1,53 @@
+def star(func):
+    def inner(*n):
+        print('*'*30)
+        func()
+        print('*'*30)
+    return inner
+def hash(func):
+    def inner(*n):
+        print('#'*30)
+        func()
+        print('#'*30)
+    return inner
+@star
+@hash
+def buisness():
+    print('Inside buisness logic')
+buisness()
+
+
+
+
+
+
+
+import sys
+sys.exit(0)
+def star(func):
+    def inner(*n):
+        print('*' * 30)
+        func()
+        print('*' * 30)
+    return inner
+
+def hash(func):
+    def inner(*n):
+        print('#' * 30)
+        func()
+        print('#' * 30)
+    return inner
+
+def dash(func):
+    def inner(*n):
+        print('-' * 30)
+        func()
+        print('-' * 30)
+    return inner
+@star
+@hash
+@dash
+def bussiness_fun():
+    print("Inside Bussiness Function")
+
+bussiness_fun()
